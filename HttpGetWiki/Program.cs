2ba9bc68b3be3e16e3a33d4adb2d203338107e0f
Aliaksandra <aleksandra.vavruk@gmail.com>
2023-10-30 00:17:53 +0300

    get wiki pages

---
+++ HttpGetWiki/Program.cs
@@ -0,0 +1,15 @@
+﻿var client = new HttpClient();
+
+for (var a = 'a'; a <= 'z'; a++)
+{
+    for (var b = 'a'; b <= 'z'; b++)
+    {
+        var response = await client.GetAsync($"https://ru.wikipedia.org/wiki/.{a}{b}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            File.WriteAllText($".\\WikiPages\\{a}{b}.html", await response.Content.ReadAsStringAsync());
+        }
+    }
+}
+
